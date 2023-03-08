b19e8a61e32d3c4443193ddfd79583bdf95156af
lichao951787328 <951787328@qq.com>
2023-03-08 09:08:35 +0800

    去除build

--- build/OpenGLConfig.cmake
+++
@@ -1,7 +0,0 @@
-set(OpenGL_INCLUDE_DIRS    "")  
-set(OpenGL_LINK_DIRS       ""   )
-
-# include paths
-include_directories(${OpenGL_INCLUDE_DIRS})
-link_directories(${OpenGL_LINK_DIRS})
-message("--[OpenGL]: Package found! Include ${OpenGL_INCLUDE_DIRS} and ${OpenGL_LINK_DIRS}" )
